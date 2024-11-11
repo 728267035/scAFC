996ea789e8f5e00c8f1c0159cb5b6073a5c18d8e
728267035 <96816334+728267035@users.noreply.github.com>
2024-11-11 09:47:32 +0800

    Update scAFC.py

--- scAFC.py
+++ scAFC.py
@@ -276,49 +276,9 @@ if __name__ == "__main__":
     # load data
     dataset = load_data(args.name)
 
-    if args.name == 'Muraro':
-        args.n_clusters = 10
-        args.n_input = 2200
-
-    if args.name == 'Darmanis':
-        args.n_clusters = 8
-        args.n_input = 6199
-
-    if args.name == 'Pollen':
-        args.n_clusters = 11
-        args.n_input = 6347
-        
-    if args.name == 'Wang':
-        args.n_clusters = 7
-        args.n_input = 6702
-
-    if args.name == 'Baron':
-        args.n_clusters = 14
-        args.n_input = 1864
-
-    if args.name == 'Melanoma':
-        args.n_clusters = 9
-        args.n_input = 5072
-
-    if args.name == 'Romanov':
-        args.n_clusters = 7
-        args.n_input = 3878
-
     if args.name == 'Bladder':
         args.n_clusters = 4
         args.n_input = 2183
-
-    if args.name == 'Diaphragm':
-        args.n_clusters = 5
-        args.n_input = 4167
-
-    if args.name == 'Deng':
-        args.n_clusters = 6
-        args.n_input = 5605
-
-    if args.name == 'Tosches':
-        args.n_clusters = 15
-        args.n_input = 2753
 
     args.pretrain_path = 'D:/scAFC/scAFC-master/pkl/preae_{}.pkl'.format(args.name)
 
